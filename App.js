d13146b476cc52005727374302281f56ef270d42
Omar Rahman <omarrahman96@gmail.com>
2019-01-19 15:40:51 +0000

    initial push

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TakePicture from './TakePicture';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TakePicture/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
